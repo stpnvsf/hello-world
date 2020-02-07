30cc3ef8f8dad0e88670f70e475e76201b31ba68
stpnvsfj <stpnvsfj@gmail.com>
2020-02-07 18:17:06 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello+" "+world+"!");
+    }
+}
