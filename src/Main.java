eb3062f99ecb17199e31f9b865dab2fdd2e7143a
stpnvsfj <stpnvsfj@gmail.com>
2020-02-07 18:44:05 +0400

    changed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Helllo World!");
+
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+        System.out.println("Helllo World!");
+
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        System.out.println("Helllo World!");
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = "+c);
+        System.out.println("a > b = "+( a > b ));
+        System.out.println("a < b = "+( a < b ));
+
         String hello = "Hello";
         String world = "World";
         System.out.println(hello+" "+world+"!");
